c3cdd0ffa8e5aafdeefbe487c7715a8e07557f47
gebi9 <gebi9@hotmail.com>
2017-04-11 16:09:54 +0200

    actionbar implemented right way with icons

--- app/src/main/java/com/example/gruppe43/idretts_app/application/view/main/MainActivity.java
+++ app/src/main/java/com/example/gruppe43/idretts_app/application/view/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gruppe43.idretts_app.application.view.main;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -16,10 +17,21 @@ import android.widget.Toast;
 
 import com.example.gruppe43.idretts_app.R;
 import com.example.gruppe43.idretts_app.application.view.fragments.FirstPage;
+import com.example.gruppe43.idretts_app.application.view.fragments.FullActivityInfo;
+import com.example.gruppe43.idretts_app.application.view.fragments.ListOfConversations;
+import com.example.gruppe43.idretts_app.application.view.fragments.Login;
+import com.example.gruppe43.idretts_app.application.view.fragments.Messages;
+import com.example.gruppe43.idretts_app.application.view.fragments.NewActivityRegistration;
+import com.example.gruppe43.idretts_app.application.view.fragments.Player;
+import com.example.gruppe43.idretts_app.application.view.fragments.ProfileView;
+import com.example.gruppe43.idretts_app.application.view.fragments.Registration;
+import com.example.gruppe43.idretts_app.application.view.fragments.ShowAllPlayers;
+import com.example.gruppe43.idretts_app.application.view.fragments.Tabs;
+import com.example.gruppe43.idretts_app.application.view.fragments.Team;
+import com.example.gruppe43.idretts_app.application.view.fragments.Trainer;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout mDrawerLayout;
-    NavigationView mNavigationView;
     FragmentManager mFragmentManager;
     FragmentTransaction mFragmentTransaction;
 
@@ -27,18 +39,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFragmentManager = getSupportFragmentManager();
         mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.replace(R.id.containerView,new FirstPage()).commit();
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        NavigationView view = (NavigationView) findViewById(R.id.nav_view);
+        view.setNavigationItemSelectedListener(this);
         setSupportActionBar(toolbar);
-
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        mDrawerLayout.setDrawerListener(toggle);
+        mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
     }
 
@@ -61,34 +73,94 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_search) {
+        if (id == R.id.toolbar_home) {
+            Toast.makeText(this, "home!", Toast.LENGTH_LONG).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.nav_home) {
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.containerView,new Tabs()).commit();
+        }
+        if (menuItem.getItemId() == R.id.nav_messages) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Messages()).commit();
+        }
+        if (menuItem.getItemId() == R.id.nav_trainer) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Trainer()).commit();
+        }
+        if (menuItem.getItemId() == R.id.navn_player) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Player()).commit();
+        }
+        if (menuItem.getItemId() == R.id.nav_Team) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Team()).commit();
+        }
+        if (menuItem.getItemId() == R.id.nav_exit) {
+            System.exit(1);
+        }
 
-        if (id == R.id.nav_home) {
-            Toast.makeText(this, "home!", Toast.LENGTH_LONG).show();
-        } else if (id == R.id.nav_messages) {
+                /*MIDLERTIDIG navigasjon///////////////////////////////////////////////////////*/
+        if (menuItem.getItemId() == R.id.firstpage) {
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.containerView,new FirstPage()).commit();
+        }
+        if (menuItem.getItemId() == R.id.logginPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Login()).commit();
+        }
+        if (menuItem.getItemId() == R.id.registrationPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Registration()).commit();
+        }
+        if (menuItem.getItemId() == R.id.homePage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Tabs()).commit();
+        }
+        if (menuItem.getItemId() == R.id.profilePage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new ProfileView()).commit();
+        }
+        if (menuItem.getItemId() == R.id.activityInformationPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new FullActivityInfo()).commit();
+        }
+        if (menuItem.getItemId() == R.id.newActivityRegistrationPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new NewActivityRegistration()).commit();
+        }
+        if (menuItem.getItemId() == R.id.showPlayersPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new ShowAllPlayers()).commit();
+        }
+        if (menuItem.getItemId() == R.id.showTeamPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Team()).commit();
+        }
+        if (menuItem.getItemId() == R.id.messegesListPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new ListOfConversations()).commit(); /* denne fragmenten viser liste av foretatte
+                    tidligere meldinger som som står lagret, når man trykker på en .. skal den åpne for melding redigerings sisde , kan scrolles tilbake for tidligere meldinger med denne personen*/
+        }
+        if (menuItem.getItemId() == R.id.messegesEditgPage) {
+            FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+            xfragmentTransaction.replace(R.id.containerView,new Messages()).commit(); /* denne fragmenten brukes for selve meldingene, som i melding siden vårt i prototypen.*/
+        }
 
+        if (menuItem.getItemId() == R.id.nav_exit) {
+            System.exit(1);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return false;//returning true keeps the item selected, selected.
     }
-
 }
