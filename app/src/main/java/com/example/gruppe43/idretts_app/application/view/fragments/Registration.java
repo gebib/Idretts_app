a707e7cf969a31fb4674933966705d6b2615efdf
Ole-K <olekstei@gmail.com>
2017-04-17 22:54:47 +0200

    Need to read up on FragmentTransaction and FragmentManager tomorrow (18.04)

--- app/src/main/java/com/example/gruppe43/idretts_app/application/view/fragments/Registration.java
+++ app/src/main/java/com/example/gruppe43/idretts_app/application/view/fragments/Registration.java
@@ -1,37 +1,40 @@
 package com.example.gruppe43.idretts_app.application.view.fragments;
 
 import android.app.AlertDialog;
-import android.content.Context;
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gruppe43.idretts_app.R;
-import com.example.gruppe43.idretts_app.application.fragment_interfaces.FragmentActivityInterface;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import static android.R.attr.format;
 
 
 public class Registration extends Fragment implements View.OnClickListener {
-
-    private EditText nameET;
-    private EditText surNameET;
-    private EditText ageET;
-    private EditText passET;
-    private EditText rePassET;
+    
+    private EditText firstNameET, lastNameET, ageET, emailET, passET, rePassET;
+    private Boolean isValid;
     private CheckBox acceptTermsCB;
-    private FragmentActivityInterface mCallback;
 
-
-    private String name, lastName, age, pss, rPss;
-    private Boolean checkBox;
+    private String firstName, lastName, age, email, pss, rPss;
+    private Boolean checkBox; //DO change this
 
     private TextView loginLink;
     private Button createAccount;
@@ -40,52 +43,43 @@ public class Registration extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_registration, container, false);
         loginLink = (TextView) view.findViewById(R.id.linkLoginTV);
         createAccount = (Button) view.findViewById(R.id.createAccountBT);
+
         createAccount.setOnClickListener(this);
         loginLink.setOnClickListener(this);
+        ageET.setOnClickListener(this);
 
-        nameET = (EditText) view.findViewById(R.id.input_email);
-        surNameET = (EditText) view.findViewById(R.id.input_surname);
-        ageET = (EditText) view.findViewById(R.id.input_age);
-        passET = (EditText) view.findViewById(R.id.input_password);
-        rePassET = (EditText) view.findViewById(R.id.input_reEnterPassword);
+        firstNameET = (EditText) view.findViewById(R.id.input_firstName);
+        lastNameET  = (EditText) view.findViewById(R.id.input_lastName);
+        ageET       = (EditText) view.findViewById(R.id.input_age);
+        emailET     = (EditText) view.findViewById(R.id.input_email);
+        passET      = (EditText) view.findViewById(R.id.input_password);
+        rePassET    = (EditText) view.findViewById(R.id.input_reEnterPassword);
         acceptTermsCB = (CheckBox) view.findViewById(R.id.familyApproveCB);
+
         // Inflate the layout for this fragment
         return view;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        try {
-            mCallback = (FragmentActivityInterface) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement IFragmentToActivity");
-        }
-    }
-
-    @Override
     public void onClick(final View v) {
         //do what you want to do when button is clicked
-        name = nameET.getText().toString().trim();
-        lastName = surNameET.getText().toString().trim();
-        age = ageET.getText().toString().trim();
-        pss = passET.getText().toString().trim();
-        rPss = rePassET.getText().toString().trim();
-        checkBox = acceptTermsCB.isChecked();
+        firstName   = firstNameET.getText().toString().trim();
+        lastName    = lastNameET.getText().toString().trim();
+        age         = ageET.getText().toString().trim();
+        email       = emailET.getText().toString().trim();
+        pss         = passET.getText().toString().trim();
+        rPss        = rePassET.getText().toString().trim();
+        checkBox    = acceptTermsCB.isChecked();
 
 
         switch (v.getId()) {
             case R.id.createAccountBT:
-                if (checkRegistrationValidity()) {
-
-                    registerUser();
+                if (validateUser()) {
                     Toast.makeText(getActivity(), "You are registered!", Toast.LENGTH_LONG).show();
 
                 }else{
@@ -102,71 +96,52 @@ public class Registration extends Fragment implements View.OnClickListener {
                 }
                 break;
             case R.id.linkLoginTV:
-                mCallback.replaceFragmentWith(new Login());
+
                 break;
-            default:
+            case R.id.input_age: //TODO: fragmentManager/Transaction
+                DialogFragment picker = new DatePickerFragment();
+                picker.show(getFragmentManager(), "datePicker");
+                break;
         }
     }
 
-    //check for valid registration information using regX
-    private boolean checkRegistrationValidity() {
-        Boolean isValidInfo;
-        String nameRegex = "^[a-zA-Z\\s]+";
-        boolean isNumberB;
-        int ageLenth = age.length();
-        try {
-            Integer.parseInt(age);
-            isNumberB = true;
-        } catch (Exception e) {
-            isNumberB = false;
-        }
-        Boolean nameOk = name.matches(nameRegex);
-        Boolean lastNameOk = lastName.matches(nameRegex);
-        Boolean ageOk = ageLenth == 2 && isNumberB;
-
-        Boolean passOk = pss.length() > 3 ;
-        Boolean rPassOk = rPss.length() > 3;
-        Boolean completePassOk = false;
-        if(passOk && rPassOk && pss.matches(rPss)){
-            completePassOk = true;
-            passET.setBackgroundColor(Color.parseColor("#68C3FC"));
-            passET.setTextColor(Color.BLACK);
-            rePassET.setBackgroundColor(Color.parseColor("#68C3FC"));
-            rePassET.setTextColor(Color.BLACK);
-        }else{
-            passET.setBackgroundColor(Color.parseColor("#009DFF"));
-            passET.setTextColor(Color.RED);
-            rePassET.setBackgroundColor(Color.parseColor("#009DFF"));
-            rePassET.setTextColor(Color.RED);
-        }
-
-        if(!checkBox){
-            acceptTermsCB.setBackgroundColor(Color.parseColor("#009DFF"));
-        }else{
-            acceptTermsCB.setBackgroundColor(Color.parseColor("#68C3FC"));
-        }
-
-        if(nameOk && lastNameOk && ageOk && completePassOk && checkBox){
-            isValidInfo = true;
-        }else{
-            isValidInfo = false;
-            EditText values[] = {nameET,surNameET,ageET};
-            Boolean isValids[] = {nameOk,lastNameOk,ageOk};
-            for (int i = 0; i < values.length; i++) {
-                if(!isValids[i]) {
-                    values[i].setBackgroundColor(Color.parseColor("#009DFF"));
-                    values[i].setTextColor(Color.RED);
-                }else{
-                    values[i].setBackgroundColor(Color.parseColor("#68C3FC"));
-                    values[i].setTextColor(Color.BLACK);
-                }
-            }
-        }
-        return isValidInfo;
-    }
 
     //register new user if valid
-    private void registerUser() {
-        //TODO firebase
+    private boolean validateUser() {
+        Boolean isValid;
+        String nameRegEx    = "^[a-zA-ZæøåÆØÅ- ]\\w{2,20}";
+        String ageRegEx     = "(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[012])-((19|20|21)\\\\d\\\\d)";
+        String passRegEx    = "^.{6,}$";
+        //format = new SimpleDateFormat("dd/MM/YYYY");
+        //String ageFormat = ageET.getText().toString();
+
+        if (!firstName.matches(nameRegEx)){
+            firstNameET.setError(getString(R.string.first_name_reg));
+            isValid = false;
+
+        } else if (!lastName.matches(nameRegEx)){
+            firstNameET.setError(getString(R.string.last_name_reg));
+            isValid = false;
+        //TODO: remove after DatePickerFragment communicates
+        } else if (!age.matches(ageRegEx) /* FORMATTEST*/ ){
+            ageET.setError(getString(R.string.age_reg));
+            isValid = false;
+
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            ageET.setError(getString(R.string.email_reg));
+            isValid = false;
+
+        } else if (!pss.matches(passRegEx)){
+            passET.setError(getString(R.string.password_reg));
+            isValid = false;
+
+        } else (!pss.matches(rPss)){
+            passET.setError(getString(R.string.password_must_match));
+            isValid = false;
+        }
+        return isValid;
+
+
+
     }
 }
